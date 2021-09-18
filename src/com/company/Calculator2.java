5328eb2ef78336f5f0c311a1cd92b3747357ede1
Adrian <grimm3960@gmail.com>
2021-09-18 18:40:22 +0300

    Add New Calculator

---
+++ src/com/company/Calculator2.java
@@ -0,0 +1,68 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Calculator2 {
+    private  String[] tokens;
+    private  int pos;
+
+    public static void main(String[] args) {
+try {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Введите свое выражение через пробел: ");
+    String expr = scanner.nextLine();
+    scanner.close();
+    Calculator2 calculator = new Calculator2(expr);
+    System.out.println(expr + " = " + calculator.calculate());
+}catch (Exception e){
+    System.out.println("Не верный формат ввода!");
+}
+    }
+
+    public Calculator2(String expr) {
+        this.tokens = expr.split(" ");
+        this.pos = 0;
+    }
+
+    public double calculate(){
+        double first = multiply();
+
+        while (pos < tokens.length) {
+            String operator = tokens[pos];
+            if (!operator.equals("+") && !operator.equals("-")){
+                break;
+            }else {
+                pos++;
+            }
+
+            double second = multiply();
+            if (operator.equals("+")){
+                first += second;
+            }else {
+                first -= second;
+            }
+        }
+        return first;
+    }
+
+    public double multiply(){
+        double first = Double.parseDouble(tokens[pos++]);
+
+        while (pos < tokens.length) {
+            String operator = tokens[pos];
+            if (!operator.equals("*") && !operator.equals("/")){
+                break;
+            }else {
+                pos++;
+            }
+
+            double second = Double.parseDouble(tokens[pos++]);
+            if (operator.equals("*")){
+                first *= second;
+            }else {
+                first /= second;
+            }
+        }
+        return first;
+    }
+}
