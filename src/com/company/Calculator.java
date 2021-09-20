05244f143a2d05dd52e5c12294148dc90ad27269
Adrian <grimm3960@gmail.com>
2021-09-20 21:04:55 +0300

    Changes

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -3,18 +3,44 @@ package com.company;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
     public static void main(String[] args) {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            ScriptEngineManager mgr = new ScriptEngineManager();
-            ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        Scanner scanner = new Scanner(System.in);
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+
+        while (true) {
             System.out.print("Введите выражение: ");
-            String str = scanner.next();
-            System.out.println(engine.eval(str));
-        } catch (Exception e) {
-            System.out.println("Неверный формат строки!!!!");
+            String exp = scanner.nextLine();
+
+            if (CheckValidation(exp)) {
+                try {
+                    System.out.print("Результат: " + engine.eval(exp));
+                    break;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
+    }
+
+    public static boolean CheckValidation(String expression) {
+        String zeroRegex = "/0";
+        String validateRegex = "^-?\\d+\\s*[+-/*]\\s*(\\(-(\\d+|\\d+\\.\\d+)\\)|(\\d+|\\d+\\.\\d+))\\s*([+\\-*/]\\s*(\\(-(\\d+|\\d+\\.\\d+)\\)|(\\d+|\\d+\\.\\d+))+\\s*)*$";
+        Pattern pattern = Pattern.compile(zeroRegex);
+        Matcher matcher = pattern.matcher(expression);
+
+        if (!Pattern.matches(validateRegex, expression)) {
+            System.out.println("Не верный формат, попробуйте еще раз");
+            return false;
+        } else if (matcher.find()) {
+            System.out.println("Попытка делить на 0");
+            return false;
+        }
+
+        return true;
     }
 }
